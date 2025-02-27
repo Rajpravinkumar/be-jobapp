dcbf270f5a1bb23878460bfed1e7ffe830ed58ad
Rajpravinkumar <rajpravinkumaras@gmail.com>
2025-02-27 23:27:54 +0530

    backend job app protal done

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const cors = require("cors");
+const authRouter = require("./routes/authRoutes");
+const userRouter = require("./routes/userRoutes");
+const adminRouter = require("./routes/adminRoutes");
+const recruiterRouter = require("./routes/recruiterRoutes");
+const errorRoutes = require("./utils/errorRotues");
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan("dev"));
+app.use(
+  cors({
+    origin: "fe-jobsapp-pro.netlify.app",
+    credentials: true,
+  })
+);
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/admin", adminRouter);
+app.use("/api/v1/recruiter", recruiterRouter);
+
+app.use(errorRoutes);
+
+module.exports = app;
